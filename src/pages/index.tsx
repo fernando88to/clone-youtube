4bf7bcdd1b6e03fcf21b739554d1faec16271489
fernando <fernando88to@gmail.com>
2022-10-27 10:02:57 -0300

    .

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+      <Layout  title="Primeira Pagina">
+        <h1>OI</h1>
+      </Layout>
+  );
+}
